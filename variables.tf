dba20ae27166c20cbb718983760ed507152880cf
Jakob Pfeiffer <pgp-jkp@pfeiffer.ws>
2021-01-03 13:04:04 +0100

    Syntax error

--- variables.tf
+++ variables.tf
@@ -5,11 +5,11 @@ variable "hcloud_token" {}
 variable "fqdn" {}
 
 variable "server_type" {
-  default "cx11"
+  default = "cx11"
 }
 
 variable "location" {
-  default "nbg1"
+  default = "nbg1"
 }
 
 variable "ssh_key_name" {}
